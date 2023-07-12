8fb292f22400114119959b006c9f7938684456c7
Tom Payne <tom@tompayne.dev>
2023-07-12 23:41:29 +0100

    Added queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,49 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = head.next;
+
+        head.next = undefined;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
