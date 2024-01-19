0778dea803874f9149975ff7cb1d7cbea873702c
Tom Payne <tom@tompayne.dev>
2024-01-19 15:12:05 +0000

    Implemented quicksort & maze solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,72 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+];
 
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    path: Point[],
+    seen: boolean[][],
+): boolean {
+    // base case: off map
+    if (
+        curr.x < 0 ||
+        curr.y < 0 ||
+        curr.x > maze[0].length - 1 ||
+        curr.y > maze[0].length - 1
+    ) {
+        return false;
+    }
+    // base case: wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+    // base case: end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(curr);
+        return true;
+    }
+    // base case: been here
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    for (let i = 0; i < directions.length; ++i) {
+        const [x, y] = directions[i];
+        if (
+            walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, path, seen)
+        ) {
+            return true;
+        }
+    }
+
+    path.pop();
+
+    return false;
+}
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    let path: Point[] = [];
+
+    let seen: boolean[][] = [];
+    for (let i = 0; i < maze.length; ++i) {
+        seen[i] = new Array(maze[0].length).fill(false);
+    }
+
+    walk(maze, wall, start, end, path, seen);
+
+    return path;
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,35 @@
-export default function quick_sort(arr: number[]): void {
+const qs = (arr: number[], lo: number, hi: number): void => {
+    if (lo >= hi) return;
 
+    const pivot = partition(arr, lo, hi);
+
+    qs(arr, lo, pivot - 1);
+    qs(arr, pivot + 1, hi);
+};
+const partition = (arr: number[], lo: number, hi: number): number => {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+
+    // [8,3,2,4,5]
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    // [3,2,4,8,5]
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+    // [3,2,4,5,8]
+    return idx;
+};
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
